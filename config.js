32cb21adceb2f8a4f504700ff99a32ddd02d053f
Saadogasia <141914674+Saadogasia@users.noreply.github.com>
2024-08-04 14:45:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAD2tCrZwxVQ8TeyZ06RJSdWDN4aYie1_o",
-  authDomain: "alfallah-93c82.firebaseapp.com",
-  projectId: "alfallah-93c82",
-  storageBucket: "alfallah-93c82.appspot.com",
-  messagingSenderId: "387253770174",
-  appId: "1:387253770174:web:dcc5e807694c26db3d4bef",
+  apiKey: {yourdata}, 
+  authDomain: {yourdata},
+  projectId: {yourdata},
+  storageBucket: {yourdata},
+  messagingSenderId: {yourdata},
+  appId: {yourdata},
 };
 
 // Initialize Firebase
